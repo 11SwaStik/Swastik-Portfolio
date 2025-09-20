d1113d2cb7bf317e85757340661f00547af82570
11SwaStik <11swastiksharma@gmail.com>
2025-09-20 19:05:26 +0530

    Create loading.jsx

---
+++ src/Components/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading(){
+  return (
+    <div className="loader">
+      <h1 className="typewriter">Booting terminal...</h1>
+      <div className="scan"></div>
+      <small>loading modules</small>
+    </div>
+  )
+}
