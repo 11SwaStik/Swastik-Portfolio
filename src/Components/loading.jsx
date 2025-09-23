dbece9f1da917484c0b5bc59a069933bc6844e60
11SwaStik <11swastiksharma@gmail.com>
2025-09-23 17:18:13 +0530

    Update loading.jsx

--- src/Components/loading.jsx
+++ src/Components/loading.jsx
@@ -1,9 +1,9 @@
 export default function Loading(){
   return (
     <div className="loader">
-      <h1 className="typewriter">Booting terminal...</h1>
+      <h1 className="typewriter">Booting system...</h1>
       <div className="scan"></div>
-      <small>loading modules</small>
     </div>
   )
 }
+
